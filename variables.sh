98f56ef107a1e46f9110ff0b9e31dbe3bf89082e
Pratyay Dhond <dhondpratyay@gmail.com>
2023-01-19 15:28:42 +0530

    ShellScripts moved to proper destination directory

--- variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-greeting="Hello There"
-user=$(whoami)
-day=$(date +%A)
-
-echo "$greeting $user! Today is $day, which happens to be the best day for you!"
-echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
